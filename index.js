2d5ce1751a5d4ba495456b55a9c11b0a308ff4df
kriper <143851863+kriper12@users.noreply.github.com>
2024-10-16 17:48:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log(`
 `);
 
 const server = http.createServer();
-const url = "https://3kh0.github.io";
+const url = "https://duckduckgo.com/50x.html?e=2";
 const PORT = process.env.PORT || 8080;
 
 server.on("request", async (req, res) => {
